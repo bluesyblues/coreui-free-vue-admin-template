ddead589641bfdf9f967115d4133500250c88311
bluesyblues <bluesyblues@gmail.com>
2022-03-03 11:43:25 +0000

    add user>

--- src/views/AddUser.vue
+++ src/views/AddUser.vue
@@ -1,154 +1,151 @@
 <template>
-  Username
-  <CFormInput
-    v-model="usernameInput"
-    aria-label="usernameInput"
-    style="width: 300px"
-  />
-  Email
-  <CFormInput
-    v-model="emailInput"
-    aria-label="emailInput"
-    style="width: 300px"
-  />
-  Password
-  <CFormInput
-    v-model="passwordInput"
-    type="password"
-    aria-label="passwordInput"
-    style="width: 300px"
-  />
-  Password validation
-  <CFormInput
-    v-model="passwordValidationInput"
-    type="password"
-    aria-label="passwordValidationInput"
-    style="width: 300px"
-  />
-  Privacy policy select
-  <CFormSelect
-    v-model="privacyPolicyAgreementSelect"
-    aria-label="privacyPolicyAgreementSelect"
-    style="width: 300px"
-    :options="[
-      'Select',
-      { label: '0', value: '0' },
-      { label: '1', value: '1' },
-    ]"
-  >
-  </CFormSelect>
-  Marketing Agreement Select
-  <CFormSelect
-    v-model="marketingAgreementSelect"
-    aria-label="marketingAgreementSelect"
-    style="width: 300px"
-    :options="[
-      'Select',
-      { label: '0', value: '0' },
-      { label: '1', value: '1' },
-    ]"
-  >
-  </CFormSelect>
-  First name
-  <CFormInput
-    v-model="firstNameInput"
-    aria-label="firstNameInput"
-    style="width: 300px"
-  />
-  Last name
-  <CFormInput
-    v-model="lastNameInput"
-    aria-label="lastNameInput"
-    style="width: 300px"
-  />
-  Company name
-  <CFormInput
-    v-model="companyInput"
-    aria-label="companyInput"
-    style="width: 300px"
-  />
-  Compression Toolket Authority
-  <CFormSelect
-    v-model="compressionToolkitAuthoritySelect"
-    aria-label="compressionToolkitAuthoritySelect"
-    style="width: 300px"
-    :options="[
-      'Select',
-      { label: '0', value: '0' },
-      { label: '1', value: '1' },
-      { label: '2', value: '2' },
-      { label: '3', value: '3' },
-      { label: '4', value: '4' },
-    ]"
-  >
-  </CFormSelect>
-  Model Search Authority
-  <CFormSelect
-    v-model="modelSearchAuthoritySelect"
-    aria-label="modelSearchAuthoritySelect"
-    style="width: 300px"
-    :options="[
-      'Select',
-      { label: '0', value: '0' },
-      { label: '1', value: '1' },
-      { label: '2', value: '2' },
-      { label: '3', value: '3' },
-      { label: '4', value: '4' },
-    ]"
-  >
-  </CFormSelect>
-  Device Farm Authority
-  <CFormSelect
-    v-model="deviceFarmAuthoritySelect"
-    aria-label="deviceFarmAuthoritySelect"
-    style="width: 300px"
-    :options="[
-      'Select',
-      { label: '0', value: '0' },
-      { label: '1', value: '1' },
-      { label: '2', value: '2' },
-      { label: '3', value: '3' },
-      { label: '4', value: '4' },
-    ]"
-  >
-  </CFormSelect>
-  Active
-  <CFormSelect
-    v-model="isActiveSelect"
-    aria-label="isActiveSelect"
-    style="width: 300px"
-    :options="[
-      'Select',
-      { label: '0', value: '0' },
-      { label: '1', value: '1' },
-    ]"
-  >
-  </CFormSelect>
-  Admin
-  <CFormSelect
-    v-model="isAdminSelect"
-    aria-label="isAdminSelect"
-    style="width: 300px"
-    :options="[
-      'Select',
-      { label: '0', value: '0' },
-      { label: '1', value: '1' },
-    ]"
-  >
-  </CFormSelect>
-  <CButton
-    align="right"
-    color="primary"
-    :disabled="
-      usernameInput.length < 1 ||
-      emailInput.length < 1 ||
-      passwordInput.length < 1 ||
-      passwordInput !== passwordValidationInput
-    "
-    @click="addUser"
-  >
-    Add
-  </CButton>
+  <div class="bg-light d-flex flex-row align-items-center">
+    <CContainer>
+      <CCard>
+        <CRow class="justify-content-center">
+          <CCol :md="6">
+            <CCard class="p-4">
+              <h5>Required</h5>
+              Username
+              <CFormInput v-model="usernameInput" aria-label="usernameInput" />
+              Email
+              <CFormInput v-model="emailInput" aria-label="emailInput" />
+              Password
+              <CFormInput
+                v-model="passwordInput"
+                type="password"
+                aria-label="passwordInput"
+              />
+              Password validation
+              <CFormInput
+                v-model="passwordValidationInput"
+                type="password"
+                aria-label="passwordValidationInput"
+              />
+              Active
+              <CFormSelect
+                v-model="isActiveSelect"
+                aria-label="isActiveSelect"
+                :options="[
+                  { label: 'Select', value: null },
+                  { label: '0', value: '0' },
+                  { label: '1', value: '1' },
+                ]"
+              >
+              </CFormSelect>
+              Privacy policy select
+              <CFormSelect
+                v-model="privacyPolicyAgreementSelect"
+                aria-label="privacyPolicyAgreementSelect"
+                :options="[
+                  { label: 'Select', value: null },
+                  { label: '0', value: '0' },
+                  { label: '1', value: '1' },
+                ]"
+              >
+              </CFormSelect>
+              Marketing Agreement Select
+              <CFormSelect
+                v-model="marketingAgreementSelect"
+                aria-label="marketingAgreementSelect"
+                :options="[
+                  { label: 'Select', value: null },
+                  { label: '0', value: '0' },
+                  { label: '1', value: '1' },
+                ]"
+              >
+              </CFormSelect>
+            </CCard>
+          </CCol>
+          <CCol :md="6">
+            <CCard class="p-4">
+              <h5>Optional</h5>
+              First name
+              <CFormInput
+                v-model="firstNameInput"
+                aria-label="firstNameInput"
+              />
+              Last name
+              <CFormInput v-model="lastNameInput" aria-label="lastNameInput" />
+              Company name
+              <CFormInput v-model="companyInput" aria-label="companyInput" />
+              Compression Toolket Authority
+              <CFormSelect
+                v-model="compressionToolkitAuthoritySelect"
+                aria-label="compressionToolkitAuthoritySelect"
+                :options="[
+                  { label: 'Select', value: null },
+                  { label: '0', value: '0' },
+                  { label: '1', value: '1' },
+                  { label: '2', value: '2' },
+                  { label: '3', value: '3' },
+                  { label: '4', value: '4' },
+                ]"
+              >
+              </CFormSelect>
+              Model Search Authority
+              <CFormSelect
+                v-model="modelSearchAuthoritySelect"
+                aria-label="modelSearchAuthoritySelect"
+                :options="[
+                  { label: 'Select', value: null },
+                  { label: '0', value: '0' },
+                  { label: '1', value: '1' },
+                  { label: '2', value: '2' },
+                  { label: '3', value: '3' },
+                  { label: '4', value: '4' },
+                ]"
+              >
+              </CFormSelect>
+              Device Farm Authority
+              <CFormSelect
+                v-model="deviceFarmAuthoritySelect"
+                aria-label="deviceFarmAuthoritySelect"
+                :options="[
+                  { label: 'Select', value: null },
+                  { label: '0', value: '0' },
+                  { label: '1', value: '1' },
+                  { label: '2', value: '2' },
+                  { label: '3', value: '3' },
+                  { label: '4', value: '4' },
+                ]"
+              >
+              </CFormSelect>
+              Admin
+              <CFormSelect
+                v-model="isAdminSelect"
+                aria-label="isAdminSelect"
+                :options="[
+                  { label: 'Select', value: null },
+                  { label: '0', value: '0' },
+                  { label: '1', value: '1' },
+                ]"
+              >
+              </CFormSelect>
+            </CCard>
+          </CCol>
+        </CRow>
+      </CCard>
+      <CRow class="justify-content-center">
+        <CButton
+          align="right"
+          color="primary"
+          class="btn btn-primary gap-2 col-5"
+          :disabled="
+            usernameInput.length < 1 ||
+            emailInput.length < 1 ||
+            passwordInput.length < 1 ||
+            passwordInput !== passwordValidationInput ||
+            privacyPolicyAgreementSelect === null ||
+            marketingAgreementSelect === null
+          "
+          @click="addUser"
+        >
+          Add
+        </CButton>
+      </CRow>
+    </CContainer>
+  </div>
 </template>
 <script>
 import axios from 'axios'
@@ -243,6 +240,11 @@ export default {
           this.$router.go()
         })
       }
+    },
+  },
+  watch: {
+    compressionToolkitAuthoritySelect: function () {
+      console.log(this.compressionToolkitAuthoritySelect)
     },
   },
 }
